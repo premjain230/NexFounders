00d410e9b683bd690f0e7817d5919f8ab7730593
premjain <premjains106@gmail.com>
2026-05-30 01:32:47 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 import { auth, db } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"; 
 import { 
   collection, addDoc, doc, getDoc, getDocs,
   query, orderBy, serverTimestamp, updateDoc,
